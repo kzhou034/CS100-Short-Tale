c2c71290550487460d57b2c8fc450b40cc2dc937
DooDooBard <jhu113@ucr.edu>
2022-05-15 20:34:38 -0700

    Set mob level

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Character.hpp"
+using namespace std;
+
+int main() {
+
+Enemy mob1;
+  mob1.setlevel(1);
+Enemy mob2;
+  mob2.setlevel(2);
+Enemy mob3;
+  mob3.setlevel(3);
+Enemy mob4;
+  mob4.setlevel(4);
+Enemy mob5;
+  mob5.setlevel(5);
+}
