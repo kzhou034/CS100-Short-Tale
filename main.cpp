16a72c590e25c58a73d9337b06597496696ef70e
Joseph Hu <77127695+DooDooBard@users.noreply.github.com>
2022-06-02 16:47:38 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,246 +2,176 @@
 #include <string>
 #include <unistd.h>
 #include "Character.hpp"
+#include "gamefunc.h"
+#include "gamefunc.cpp"
 using namespace std;
 
-void knightSkills(Character* player, Character* enemy){
-  bool isshieldraised = player->getStatus();
-  string answer;
-  system("clear");
-  cout << "Available Skills: " << endl;
-  cout << "1) Basic Attack" << endl;
-  if(!isshieldraised){
-    cout << "2) Raise Shield" << endl;
-    cout << "        Increase defense by 50% but reduces attack damage" << endl;
-  }
-  else{
-    cout << "2) Lower Shield" << endl;
-  }
-  if(player->getCD() == 0){
-    cout << "3) Holy Smite" << endl;
-    cout << "        Smites the enemy in a holy light." << endl;
-  }
-  else{
-    cout << "3) Holy Smite " << "(Cooldown of " << player->getCD()  << " turn/s."<<endl;
-    cout << "        Smites the enemy in a holy light." << endl;
-  }
-  cin >> answer;
-  while(answer != "1" && answer != "2" && answer != "3"){   
-      cout << "\nThat doesn't seem to be a valid answer. Please try again.\n" << endl;
-      sleep(2);
-      system("clear");
-      cout << "Available Skills: " << endl;
-      cout << "1) Basic Attack" << endl;
-      if(!isshieldraised){
-        cout << "2) Raise Shield" << endl;
-        cout << "        Increase defense by 50% but reduces attack damage" << endl;
-      }
-      else{
-        cout << "2) Lower Shield" << endl;
-      }
-      if(player->getCD() == 0){
-        cout << "3) Holy Smite" << endl;
-        cout << "        Smites the enemy in a holy light." << endl;
-      }
-      else{
-        cout << "3) Holy Smite " << "(Cooldown of " << player->getCD()  << " turn/s)"<<endl;
-        cout << "        Smites the enemy in a holy light." << endl;
-      }
-      answer = "";
-      cin >> answer;
-      system("clear");
-  }
-    system("clear");
-    if(answer == "3"){
-      if(player->getCD() == 0){
-        player->holysmite(enemy);
-        cout << "You conjure upon a holy magic to smite the " << enemy->get_name() << "." << endl;
-        player->setCD();
-        player->setCD();
-        player->setCD();
-      }
-      else{
-
-        while(answer != "1" && answer != "2"){   
-          cout << "You are unable to cast holy smite. Please choose another skill.\n" << endl;
-          sleep(2);
-          cout << "Available Skills: " << endl;
-          cout << "1) Basic Attack" << endl;
-          if(!isshieldraised){
-            cout << "2) Raise Shield" << endl;
-            cout << "        Increase defense by 50% but reduces attack damage" << endl;
-          }
-          else{
-            cout << "2) Lower Shield" << endl;
-          }
-          answer = "";
-          cin >> answer;
-          system("clear");
-        }
-      }
-    }
-    if(answer == "1"){
-      cout << "You attack the " << enemy->get_name() << "." << endl;
-      player->atk(enemy);
-      cout << endl;
-      sleep(2);
-    }
-    if(answer == "2"){
-      if(!isshieldraised){
-        player->raiseshield();
-        isshieldraised = true;
-        cout << "You raise your shield against the " << enemy->get_name() << "." << endl;
-      }
-      else{
-        player->lowershield();
-        isshieldraised = false;
-        cout << "You lower your shield against the " << enemy->get_name() << "." << endl;
-      }
-    }
-}
-
-bool inCombat(Character* player, Character* enemy){
-  int turnNum = 1;
-  bool isVictory = true;
-  string answer;
-  while(isVictory){
-    cout << "[Turn " << turnNum << "]\n" << endl;
-    enemy->print();
-    cout << "\nYou currently have " << player->get_health() << " health.\n" << endl;
-    cout << "Options:" << endl;
-    cout << "Press 1 to attack." << endl;
-    cout << "Press 2 to use skills." << endl;
-    cout << "Press 3 to use items." << endl;
-    cout << "Press 4 to see your stats." << endl;
-    cout << "Press 5 to run away." << endl;
-    cin >> answer;
-    while(answer != "1" && answer != "2" && answer != "3" && answer != "4" && answer != "5"){   
-        cout << "\nThat doesn't seem to be a valid answer. Please try again.\n" << endl;
-        sleep(2);
-        system("clear");
-        cout << "Name: " << enemy->get_name() << "\nType: " << enemy->get_type() << "\nHealth: " << enemy->get_health() << endl << endl << endl;
-        cout << "You currently have " << player->get_health() << " health." << endl;
-        cout << "Options:" << endl;
-        cout << "Press 1 to attack." << endl;
-        cout << "Press 2 to use skills." << endl;
-        cout << "Press 3 to use items." << endl;
-        cout << "Press 4 to see your stats." << endl;
-        cout << "Press 5 to run away." << endl;
-        answer = "";
-        cin >> answer;
-    }
-    if (answer == "1") {
-        cout << "\nYou attack the " << enemy->get_name() << "." << endl;
-        player->atk(enemy);
-        cout << endl;
-        sleep(2);
-        if(enemy->get_health() <= 0){
-          return true;
-        }
-        else{
-          enemy->action(player);
-          cout << "Press any key to continue." << endl;
-          cin >> answer;
-          system("clear");
-        }
-    }
-    else if (answer == "2") {
-      if(player->get_type() == "Knight"){
-        knightSkills(player, enemy);
-      }
-      cout << endl;
-        sleep(2);
-        if(enemy->get_health() <= 0){
-          return true;
-      }
-      else{
-        enemy->action(player);
-        cout << "Press any key to continue." << endl;
-        cin >> answer;
-        system("clear");
-      }
-    }
-      
-    else if (answer == "3") {
-        cout << "You open inventory" << endl;
-      
-        system("clear");
-    }
-    else if (answer == "4") {
-        system("clear");
-        cout << "Name: " << player->get_name() << endl;
-        cout << "Class: " << player->get_type() << endl;
-        cout << "Level: " << player->get_level() << endl;
-        cout << "Attack: " << player->get_attack() << endl;
-        cout << "Defense: " << player->get_defense() << endl;
-        cout << "Resistance: "<< player->get_resistance() << endl;
-        cout << "Speed: " << player->get_speed() << endl;
-        cout << "\nPress any key to return. " << endl;
-        cin >> answer;
-        turnNum--;
-        player->setCD();
-        system("clear");
-    }
-
-    else if (answer == "5") {
-        cout << "You try to run away." << endl;
-        system("clear");
-        return false;
-    }
-    turnNum++;
-    if(player->getCD() != 0){
-      player->redCD();
-    }
-  }
-  return true;
-}
-
-void PrintChapter1(Character* player, Character* enemy){
-  Character* userPlay = player;
-  Character* userEnemy = enemy;
-  
-  cout << "Stage 1: The Beginning." << endl;
-    string answer = "";
-
-    cout << "Leaving your hometown in pursuit of the Princess, you trek across the long roads across the mountains with your horse." << endl;
-
-    cout << "While passing by an abandoned settlement, you are attacked by a wild monkey." << endl;
-
-    cout << "Press any key to continue." << endl;
-    cin >> answer;
-    
-    system("clear");
-    bool isVictory = inCombat(userPlay, userEnemy);
-    if(isVictory){
-      cout <<  "You defeat the monkey." << endl;
-    }
-    else{
-      cout << "You are defeated by the monkey." << endl;
-    }
-
-    sleep(10);
-    system("clear");
-}
-
-
 int main(){
-    string name = "Joseph";
+    string name = "";
     string answer;
 
     Character* enemy1;
-    enemy1 = new Enemy("Monkey", "Beast", 1, 10, 3, 2, 2, 5);
-    enemy1->setlevel(10);
-    Character* playerClass;
-
-    playerClass = new Knight(name, "Knight", 10, 6, 8, 6, 4);
-    playerClass->setlevel(10);
-    PrintChapter1(playerClass, enemy1);
-    // PrintChapter2();
-    // PrintChapter3();
-    // PrintChapter4();
-    // PrintChapter5();
-    // PrintChapter6();
+    enemy1 = new Enemy("Monkey", "Beast", 1, 10, 1, 2, 2, 5);
+  
+    Character* enemy2;
+    enemy2 = new Enemy("Goblin", "Beast", 1, 11, 2, 2, 2, 5);
+    (*enemy2).setlevel(2);
     
+    
+    Character* enemy3;
+    enemy3 = new Enemy("Bandit", "Human", 1, 12, 2, 2, 2, 5);
+  (*enemy3).setlevel(3);
+  
+    Character* enemy4;
+    enemy4 = new Enemy("Red Demon", "Demon", 1, 12, 2, 2, 2, 5);
+  (*enemy4).setlevel(4);
+  
+    Character* enemy5;
+    enemy5 = new Enemy("Blue Demon", "Demon", 1, 13, 2, 2, 2, 5);
+  (*enemy5).setlevel(5);
+  
+    Character* enemy6;
+    enemy6 = new Enemy("Demon Knight", "Demon", 1, 13, 3, 2, 2, 5);
+  (*enemy6).setlevel(6);
+  
+    Character* enemy7;
+    enemy7 = new Enemy("Demon Lord", "Demon", 1, 15, 3, 2, 2, 5);
+  (*enemy7).setlevel(7);
+
+    cout << "Welcome to Short Tale! The princess has been kidnapped, and there was nothing we could do to stop it!" << endl;
+    sleep(1.5);
+    cout << "She has been taken by hordes of monsters, across many lands." << endl;
+    sleep(1.5);
+    cout << "You are the only hero strong enough to save her!" << endl;
+    cout << endl;
+    sleep(3);
+    cout << "Before you embark on your mission, what is your name?" << endl;
+    cin >> name;
+    cout << endl;
+    system("clear");
+    cout << "I see. So your name is " << name << "?" << endl;
+    cout << "Press 1 for Yes and 2 for No." << endl;
+    cin >> answer;
+    system("clear");
+
+    while(answer != "1" && answer != "2"){
+        cout << "That doesn't seem to be a valid answer. Please press 1 for Yes and 2 for No." << endl;
+        answer = "";
+        cin >> answer;
+        system("clear");
+    }
+
+
+    while(answer == "2"){
+        cout << "I see. Once again, what is your name?" << endl;
+        name = "";
+        cin >> name;
+        system("clear");
+        cout << "I see. So your name is " << name << "?" << endl;       //rechecks name
+        cout << "Press 1 for Yes and 2 for No." << endl;                //redoes confirmation line
+        answer = "";
+        cin >> answer;
+        system("clear");
+        while(answer != "1" && answer != "2"){   //re-asks if invalid answer is given
+            cout << "That doesn't seem to be a valid answer. Please press 1 for Yes and 2 for No." << endl;
+            answer = "";
+            cin >> answer;
+          system("clear");
+        }
+    }
+    
+
+    sleep(1);
+    cout << endl;
+    system("clear");
+    cout << "I see, so you're " << name << "!" << endl;
+    sleep(2);
+    cout << "Well, " << name << ", I don't seem to remember what your class was. Can you please remind me?" << endl;
+    sleep(3);
+    cout << "If I recall, you were a..." << endl;
+    sleep(4);
+    cout << endl;
+    cout << "I'm sorry, what class were you again?" << endl;
+    sleep(1.5);
+
+
+    cout << "Options:" << endl;
+    cout << "1: I am a chivalrous knight. I am balanced and an all-rounder. (Press 1 to be assigned Knight class)" << endl;
+    sleep(3);
+    cout << endl;
+    cout << "2: I am a wise mage. I prefer mana and magic attacks. (Press 2 to be assigned the Mage class)" << endl;
+    sleep(3);
+    cout << endl;
+    cout << "3: I am a swift rogue. I boast high speed and attack. (Press 3 to be assigned the Rogue class)" << endl;
+    cout << endl;
+    sleep(3);
+    cout << "PLEASE NOTE: Once you make your decision, there is no going back or changing your mind." << endl;
+
+
+    answer = "";
+    cin >> answer;
+    system("clear");
+    Character* playerClass;
+  
+    while(answer != "1" && answer != "2" && answer != "3"){   
+        cout << "That doesn't seem to be a valid answer. Please try again.\n" << endl;
+        cout << "Options:" << endl;
+        cout << "1: I am a chivalrous knight. I am balanced and an all-rounder. (Press 1 to be assigned Knight class)" << endl;
+        sleep(1);
+        cout << endl;
+        cout << "2: I am a wise mage. I prefer mana and magic attacks. (Press 2 to be assigned the Mage class)" << endl;
+        sleep(1);
+        cout << endl;
+        cout << "3: I am a swift rogue. I boast high speed and attack. (Press 3 to be assigned the Rogue class)" << endl;
+        cout << endl;
+        sleep(1);
+        cout << "PLEASE NOTE: Once you make your decision, there is no going back or changing your mind." << endl;
+        answer = "";
+        cin >> answer;
+        system("clear");
+    }
+    if (answer == "1") {
+        cout << "I see. So you are a knight!" << endl;
+        playerClass = new Knight(name, "Knight", 10, 6, 8, 6, 4);
+    }
+
+    else if (answer == "2") {
+        cout << "I see. So you are a mage!" << endl;
+        playerClass = new Mage(name, "Mage", 7, 8, 3, 5, 6);
+    }
+
+    else if (answer == "3") {
+        cout << "I see. So you are a rogue!" << endl;
+        playerClass = new Rogue(name, "Rogue", 6, 8, 3, 2, 8);
+    }
+  
+    sleep(1);
+    cout << endl;
+    cout << name << " the " << playerClass->get_type() << "!" << endl;
+    cout << "I entrust you with saving the princess. Please return her safely!" << endl;
+    sleep(5);
+
+
+    system("clear");
+    PrintChapter1(playerClass, enemy1);
+    playerClass->setlevel(2);
+    PrintChapter2(playerClass, enemy2);
+    playerClass->setlevel(3);
+    PrintChapter3(playerClass, enemy3);
+    playerClass->setlevel(5);
+    PrintChapter4(playerClass, enemy4, enemy5);
+    playerClass->setlevel(6);
+    PrintChapter5(playerClass, enemy6);
+    playerClass->setlevel(7);
+    PrintChapter6(playerClass, enemy7);
+    playerClass->setlevel(8);
     delete playerClass;
     delete enemy1;
+    delete enemy2;
+    delete enemy3;
+    delete enemy4;
+    delete enemy5;
+    delete enemy6;
+    delete enemy7;
+  
 }
