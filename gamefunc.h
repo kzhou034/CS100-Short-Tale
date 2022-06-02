1ec227c39ff7fafd8787a81f8789ee9beeb02340
Joseph Hu <77127695+DooDooBard@users.noreply.github.com>
2022-06-02 16:46:47 -0700

    Update gamefunc.h

---
+++ gamefunc.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Character.hpp"
+
+void knightSkills(Character* player, Character* enemy);
+void rogueSkills(Character* player, Character* enemy);
+void mageSkills(Character* player, Character* enemy);
+
+bool inCombat(Character* player, Character* enemy);
+
+void PrintChapter1(Character* player, Character* enemy);
+void PrintChapter2(Character* player, Character* enemy);
+void PrintChapter3(Character* player, Character* enemy);
+void PrintChapter4(Character* player, Character* enemy, Character* enemy2);
+void PrintChapter5(Character* player, Character* enemy);
+void PrintChapter6(Character* player, Character* enemy);
+void PrintChapter7(Character* player, Character* enemy);
